58feec922accddab2dd02837806be3da8e43c9ee
Jordan Fontaine <jordanmarcusfontaine@gmail.com>
2024-03-04 06:36:01 +0700

    form components for the course creation

---
+++ tutoring-app/tutoring/components/courses/image-form.tsx
@@ -0,0 +1,118 @@
+"use client";
+
+import * as z from "zod";
+import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormLabel,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { FileUp, ImageIcon, Pencil, PlusCircle } from "lucide-react";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import { cn } from "@/lib/utils";
+import { Textarea } from "../ui/textarea";
+import { Course } from "@prisma/client";
+import Image from "next/image";
+import { FileUpload } from "./file-upload";
+
+interface ImageFormProps {
+  initialData: Course;
+  courseId: string;
+}
+
+const formSchema = z.object({
+  imageUrl: z.string().min(3, {
+    message: "Image is required",
+  }),
+});
+
+export const ImageForm = ({ initialData, courseId }: ImageFormProps) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const router = useRouter();
+  const toggleEdit = () => setIsEditing((current) => !current);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      imageUrl: initialData?.imageUrl || "",
+    },
+  });
+
+  const { isSubmitting, isValid } = form.formState;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+    try {
+      await axios.patch(`/api/courses/${courseId}`, values);
+      toast.success("Course Image updated successfully");
+      toggleEdit();
+      router.refresh();
+    } catch (error) {
+      toast.error("Something went wrong. Please try again.");
+    }
+  };
+
+  return (
+    <div className="mt-6 border bg-primary/20 rounded-md p-4">
+      <div className="font-medium flex items-center justify-between">
+        Course Image
+        <Button variant="ghost" onClick={toggleEdit}>
+          {isEditing && <>Cancel</>}
+          {!isEditing && !initialData.imageUrl && (
+            <>
+              <PlusCircle className="h-4 w-4 mr-2" />
+              Add an Image
+            </>
+          )}
+          {!isEditing && initialData.imageUrl && (
+            <>
+              <Pencil className="h-4 w-4 mr-2" />
+              Edit Image
+            </>
+          )}
+        </Button>
+      </div>
+      {!isEditing &&
+        (!initialData.imageUrl ? (
+          <div className="flex items-center justify-center h-60 bg-primary/30 rounded-md">
+            <ImageIcon className="h-10 w-10" />
+          </div>
+        ) : (
+          <div className="relative aspect-video mt-2">
+            <Image
+              src={initialData?.imageUrl}
+              alt="Upload"
+              fill
+              className="object-cover rounded-md"
+            />
+          </div>
+        ))}
+
+      {isEditing && (
+        <div>
+          <FileUpload
+            endpoint="courseImage"
+            onChange={(url) => {
+              if (url) {
+                onSubmit({ imageUrl: url });
+              }
+            }}
+          />
+          <div className="text-xs text-muted-foreground mt-4">
+            16:9 aspect ratio recommended
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
---
+++ tutoring-app/tutoring/components/courses/title-form.tsx
@@ -0,0 +1,113 @@
+"use client";
+
+import * as z from "zod";
+import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormLabel,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Pencil } from "lucide-react";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import { cn } from "@/lib/utils";
+
+interface TitleFormProps {
+  initialData: {
+    title: string;
+  };
+  courseId: string;
+}
+
+const formSchema = z.object({
+  title: z.string().min(3, {
+    message: "Title must be at least 3 characters",
+  }),
+});
+
+export const TitleForm = ({ initialData, courseId }: TitleFormProps) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const router = useRouter();
+  const toggleEdit = () => setIsEditing((current) => !current);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData,
+  });
+
+  const { isSubmitting, isValid } = form.formState;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+    try {
+      await axios.patch(`/api/courses/${courseId}`, values);
+      toast.success("Course title updated successfully");
+      toggleEdit();
+      router.refresh();
+    } catch (error) {
+      toast.error("Something went wrong. Please try again.");
+    }
+  };
+
+  return (
+    <div className="mt-6 border bg-primary/20 rounded-md p-4">
+      <div className="font-medium flex items-center justify-between">
+        Course Title
+        <Button variant="ghost" onClick={toggleEdit}>
+          {isEditing ? (
+            <>Cancel</>
+          ) : (
+            <>
+              <Pencil className="h-4 w-4 mr-2" />
+              Edit Title
+            </>
+          )}
+        </Button>
+      </div>
+      {!isEditing && <p className={cn(
+            "text-sm mt-2 text-primary/70 italic",
+            !initialData.title && "text-primary/30 italic"
+          )}>{initialData.title}</p>}
+
+      {isEditing && (
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-4 mt-4"
+          >
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      disabled={isSubmitting}
+                      placeholder="e.g Introduction to Biology 1"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex items-center gap-x-2">
+              <Button type="submit" disabled={!isValid || isSubmitting}>
+                Save
+              </Button>
+            </div>
+          </form>
+        </Form>
+      )}
+    </div>
+  );
+};
