c3635c6ba5f52ae678f2df3c8fa8ae1c8349cb63
Jordan Fontaine <jordanmarcusfontaine@gmail.com>
2024-03-18 21:02:29 +0700

    shadcn components

---
+++ tutoring-app/tutoring/components/home/Section.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+interface SectionProps {
+    className: string,
+    id: string,
+    crosses: string,
+    crossesOffset: string,
+    custompaddings: string
+}
+
+const Section = ({
+    className,
+    id,
+    crosses,
+    crossesOffset,
+    custompaddings
+}: SectionProps
+) => {
+    return ( 
+        <div className=>
+
+        </div>
+     );
+}
+ 
+export default Section;
